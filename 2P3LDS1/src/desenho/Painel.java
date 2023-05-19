2d7745133e4744c5220dd41f3dbfe3d4a352acc1
Valter Cinquenta <valthercinquenta@gmail.com>
2023-05-19 08:45:37 +0200

    Update Painel.java

--- 2P3LDS1/src/desenho/Painel.java
+++ 2P3LDS1/src/desenho/Painel.java
@@ -14,12 +14,12 @@ public class Painel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        g.setColor(Color.red);
-//        g.drawLine(100, 300, 500, 20);
+        g.setColor(Color.red);
+        g.drawLine(100, 300, 500, 20);
         g.setColor(Color.GREEN);
-//        g.drawRect(200, 200, 300, 300);
-//        g.fillRect(200, 200, 300, 300);
-//        g.drawArc(100, 60, 300, 150, 30, 300);
+        g.drawRect(200, 200, 300, 300);
+        g.fillRect(200, 200, 300, 300);
+        g.drawArc(100, 60, 300, 150, 30, 300);
 
         Polygon p = new Polygon();
         p.addPoint(300, 50);
